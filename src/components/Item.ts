54a403178c683601d7009e07f7f2ae6db10cb446
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-10-30 23:46:27 +0900

    main feat : board event 프레임 개발

--- src/components/Item.ts
+++ src/components/Item.ts
@@ -2,6 +2,9 @@ type BulletinBoard = {};
 
 export class bulletinBoard {
   constructor(parameters: BulletinBoard) {}
+  private board = () => {
+    console.log("board");
+  };
 }
 
 const item = `<div>MItem MItem MItem</div>`;
